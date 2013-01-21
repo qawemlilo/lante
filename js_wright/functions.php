92707d67a7028e593d158850bd84ab0566f90c61
Qawelesizwe <qawemlilo@gmail.com>
2013-01-21 12:34:26 +0200

    ...

--- js_wright/functions.php
+++ js_wright/functions.php
@@ -9,3 +9,12 @@
  
 // Restrict Access to within Joomla
 defined('_JEXEC') or die('Restricted access');
+
+
+function wright_joomla_mod_login($buffer) {
+	$buffer = preg_replace('/ id="login-form"/Ui', ' id="login-form" class="form-inline" ', $buffer);
+	$buffer = preg_replace('/<ul>/Ui', '<ul  class="nav nav-list well">', $buffer);
+	$buffer = preg_replace('/ class="button"/Ui', ' class="button btn btn-warning" ', $buffer);
+	return $buffer;
+				
+}
