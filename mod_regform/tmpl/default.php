5017bf63ed6ebd96a0def70149f3f5985462eb15
Qawelesizwe <qawemlilo@gmail.com>
2013-02-22 12:59:37 +0200

    reg form edited

--- mod_regform/tmpl/default.php
+++ mod_regform/tmpl/default.php
@@ -3,29 +3,41 @@ defined('_JEXEC') or die('Restricted access'); // no direct access
 
 ?>
 
-<form style="padding-bottom: 15px;" class="bs-docs-example">
-            <div class="controls controls-row">
-              <input type="text" placeholder="First Name" class="span3">
-              <input type="text" placeholder="Surname" class="span2">
-            </div>
+<form style="padding-left: 45px; background-color: #F0F0F0;" class="well">
+    <h1 style="color: #fd7800;">Register for FREE</h1>
+    <div class="controls controls-row">
+        <input type="text" placeholder="First Name" class="span3">
+        <input type="text" placeholder="Surname" class="span2">
+    </div>
             
-            <div class="controls">
-              <input type="text" placeholder="Email Address" class="span5">
-            </div>
+    <div class="controls">
+        <input type="text" placeholder="Email Address" class="span5">
+    </div>
 
-            <div class="controls">
-              <input type="text" placeholder="Re-enter Email Address" class="span5">
-            </div>
+    <div class="controls">
+        <input type="text" placeholder="Re-enter Email Address" class="span5">
+    </div>
             
-            <div class="controls">
-              <input type="text" placeholder="Password" class="span5">
-            </div>
+    <div class="controls">
+        <input type="text" placeholder="Password" class="span5">
+    </div>
             
-            <div class="controls">
-              <input type="text" placeholder="Re-enter Password" class="span5">
-            </div>
+    <div class="controls">
+        <input type="text" placeholder="Re-enter Password" class="span5">
+    </div>
+    
+    <p style="font-size: 12px; margin-top: 12px;">By clicking Sign Up, you agree to our Terms & Conditions</p>
             
-            <p>By clicking Sign Up, you agree to our Terms & Conditions</p>
-            
-            <p><button  class="btn btn-large btn-warning"><strong>Sign Up</strong></button></p>
+    <p><button  class="btn btn-large btn-warning" style="padding-left: 50px; padding-right: 50px; background-color: #fd7800; background-image: -moz-linear-gradient(center top , #F89406, #fd7800)"><strong>Sign Up</strong></button></p>
 </form>
+
+<div style="color: #fd7800;">
+<p>You can use LanteOTC to:</p>
+<ul>
+<li>Buy Shares from a listed company</li>
+<li>Sell your shares</li>
+<li>View trading history of listed company</li>
+<li>View trading value or worth</li>
+<li>Register or update your details as an investor</li>
+</ul>
+</div>
