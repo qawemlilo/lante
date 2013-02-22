c8792cb0c700c2143ba3d0a5ff6ac496f874e1c0
Qawelesizwe <qawemlilo@gmail.com>
2013-02-22 10:22:26 +0200

    Created reg form skeleton

---
+++ mod_regform/tmpl/default.php
@@ -0,0 +1,31 @@
+<?php 
+defined('_JEXEC') or die('Restricted access'); // no direct access 
+
+?>
+
+<form style="padding-bottom: 15px;" class="bs-docs-example">
+            <div class="controls controls-row">
+              <input type="text" placeholder="First Name" class="span3">
+              <input type="text" placeholder="Surname" class="span2">
+            </div>
+            
+            <div class="controls">
+              <input type="text" placeholder="Email Address" class="span5">
+            </div>
+
+            <div class="controls">
+              <input type="text" placeholder="Re-enter Email Address" class="span5">
+            </div>
+            
+            <div class="controls">
+              <input type="text" placeholder="Password" class="span5">
+            </div>
+            
+            <div class="controls">
+              <input type="text" placeholder="Re-enter Password" class="span5">
+            </div>
+            
+            <p>By clicking Sign Up, you agree to our Terms & Conditions</p>
+            
+            <p><button  class="btn btn-large btn-warning"><strong>Sign Up</strong></button></p>
+</form>
