bd7eb1bb4044a179a49f4c46cf59300f703ce0b6
Qawelesizwe <qawemlilo@gmail.com>
2013-02-22 10:49:00 +0200

    Created about otc module

---
+++ mod_otc/tmpl/default.php
@@ -0,0 +1,9 @@
+<?php 
+defined('_JEXEC') or die('Restricted access'); // no direct access 
+
+?>
+
+<div class="row-fluid" style="color: #fd7800; width: 340px; padding: 0px 10px 0px 10px">
+  <img border="0" alt="LanteOTC" src="/images/Slideshow%20Images-2.jpg"><br><br>
+  <strong>LanteOTC</strong> is an online market place that <strong>connects Businesses with Investors</strong> through Over The Counter <strong>online trading</strong>.
+</div>
