a9639d6a59ab912c19f41293df70c47ed37127d8
Qawelesizwe <qawemlilo@gmail.com>
2013-02-25 14:09:10 +0200

    ...

--- mod_otc/tmpl/default.php
+++ mod_otc/tmpl/default.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die('Restricted access'); // no direct access
 
 ?>
 
-<div style="color: #fd7800; width: 540px; padding-left: 5px;padding-right: 5px;">
+<div style="color: #fd7800; padding-left: 5px;padding-right: 5px;">
   <img border="0" alt="LanteOTC" style="width: 540px; border-radius: 3px; -moz-border-radius: 3px; -webkit-border-radius: 3px" src="/images/shutterstock_85769425.jpg"><br><br>
   <strong>LanteOTC</strong> is an online market place that <strong>connects Businesses with Investors</strong> through Over The Counter <strong>online trading</strong>.<br><br>
 
