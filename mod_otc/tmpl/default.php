34b2ca833421f72cf01d61c9e68d96b36aab6a7f
Qawelesizwe <qawemlilo@gmail.com>
2013-02-25 10:14:55 +0200

    ...

--- mod_otc/tmpl/default.php
+++ mod_otc/tmpl/default.php
@@ -3,7 +3,48 @@ defined('_JEXEC') or die('Restricted access'); // no direct access
 
 ?>
 
-<div style="color: #fd7800; width: 540px; padding-left: 5px;padding-right: 5px; 150px; height: 150px;">
+<div style="color: #fd7800; width: 540px; padding-left: 5px;padding-right: 5px;">
   <img border="0" alt="LanteOTC" style="width: 540px; border-radius: 3px; -moz-border-radius: 3px; -webkit-border-radius: 3px" src="/images/shutterstock_85769425.jpg"><br><br>
-  <strong>LanteOTC</strong> is an online market place that <strong>connects Businesses with Investors</strong> through Over The Counter <strong>online trading</strong>.
+  <strong>LanteOTC</strong> is an online market place that <strong>connects Businesses with Investors</strong> through Over The Counter <strong>online trading</strong>.<br><br>
+
+  <table class="table table-condensed listings">
+              <thead>
+                <tr>
+                  <th colspan="5">New Listings</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="listings-header">Stock</td>
+                  <td class="listings-header">Last</td>
+                  <td class="listings-header">CHG</td>
+                  <td class="listings-header">%CHG</td>
+                  <td class="listings-header">Time</td>
+                </tr>
+                <tr>
+                  <td>AFRISH</td>
+                  <td>10.00</td>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>1 Jan</td>
+                </tr>
+                <tr>
+                  <td>Masters Tour</td>
+                  <td>10.00</td>
+                  <td>+0.50</td>
+                  <td>0.52</td>
+                  <td>9:30</td>
+                </tr>
+                <tr>
+                  <td>TribalAff</td>
+                  <td>5.00</td>
+                  <td>0</td>
+                  <td>0</td>
+                  <td>1 Jan</td>
+                </tr>
+                <tr>
+                  <td colspan="5" class="listings-header">Market Open</td>
+                </tr>
+              </tbody>
+            </table>
 </div>
