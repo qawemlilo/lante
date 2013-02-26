ef65a89a6bfdefe180f2539f52a9e504a9f98d3e
Raging Flame <qawemlilo@gmail.com>
2013-02-26 22:02:10 +0200

    ...

--- wright/html/com_users/registration/complete.php
+++ wright/html/com_users/registration/complete.php
@@ -4,15 +4,15 @@
  * @subpackage	com_users
  * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
- * @since		1.5
+ * @since		1.6
  */
 
 defined('_JEXEC') or die;
-
-if ($this->user->get('guest')):
-	// The user is not logged in.
-	echo $this->loadTemplate('login');
-else:
-	// The user is already logged in.
-	echo $this->loadTemplate('logout');
-endif;
+?>
+<div class="registration-complete<?php echo $this->pageclass_sfx;?>">
+	<?php if ($this->params->get('show_page_heading')) : ?>
+	<h1>
+		<?php echo $this->escape($this->params->get('page_heading')); ?>
+	</h1>
+	<?php endif; ?>
+</div>
