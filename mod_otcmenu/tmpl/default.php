6a310c1c87e5a9b183b828df77ea32e6c5b2ec87
Qawelesizwe <qawemlilo@gmail.com>
2013-03-01 15:59:52 +0200

    ...

---
+++ mod_otcmenu/tmpl/default.php
@@ -0,0 +1,11 @@
+<?php 
+defined('_JEXEC') or die('Restricted access'); // no direct access 
+
+?>
+
+<ul class="nav nav-pills">
+   <li class="active"><a href="<?php echo JURI::base(); ?>">Home</a></li>
+   <li><a href="#">...</a></li>
+   <li><a href="#">...</a></li>
+   <li><a href="#">Log out</a></li>
+</ul>
